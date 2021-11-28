e4bb3b1862e57211506d0603e37e2103250c3294
odusseus <info@odusseus.org>
2021-11-28 17:20:12 +0100

    Basis Hello world

---
+++ alpha/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Osusseus.Alpha {
+  public static class Program {
+    public static void Main (string[] args){
+        Console.WriteLine("Hello, World! Here Pascal!");
+
+        var box = new Box("Test");
+
+        Console.WriteLine(box.name);
+
+    }
+  }
+} 
---
+++ alpha/box.cs
@@ -0,0 +1,13 @@
+namespace Osusseus.Alpha {
+  public class Box {
+
+    public Box (){}
+
+    public Box(string name)
+    {
+      this.name = name;
+    }
+
+    public string? name { get; set;}
+  }
+}
