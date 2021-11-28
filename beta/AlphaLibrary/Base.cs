e6fa1ba32585e3c3353ec0a25730f1548b05edf3
odusseus <info@odusseus.org>
2021-11-28 23:19:39 +0100

    String to date and debug

--- beta/AlphaLibrary/Base.cs
+++ beta/AlphaLibrary/Base.cs
@@ -15,7 +15,35 @@ namespace Cs.AlphaLibrary;
         return null;
       }
 
-      return DateTime.Today.Date;
-    }
+      string day = dateIn.Substring(0, 2);
+      int.TryParse(day, out int dayInt);
+      if(dayInt < 1 || dayInt > 31)
+      {
+        return null;
+      }
 
+      string month = dateIn.Substring(3, 2);
+      int.TryParse(month, out int monthInt);
+      if(monthInt < 1 || monthInt > 12)
+      {
+        return null;
+      }
+
+      string year = dateIn.Substring(6, 4);
+      int.TryParse(year, out int yearInt);
+      if(yearInt < 1 || yearInt > 9999)
+      {
+        return null;
+      }
+
+      try
+      {
+        DateTime dateTime = new DateTime(yearInt, monthInt, dayInt);
+        return dateTime;
+      }
+      catch
+      {
+        return null;
+      }
+    }
   }
--- beta/readme.md
+++ beta/readme.md
@@ -1,1 +1,3 @@
 https://docs.microsoft.com/nl-nl/dotnet/core/tutorials/library-with-visual-studio-code?pivots=dotnet-6-0
+
+dotnet run --project .\ShowCase\ShowCase.csproj
