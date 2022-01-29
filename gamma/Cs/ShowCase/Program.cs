62adb6e141f9db0335a9ed392d4237ace56797f8
odusseus <info@odusseus.org>
2022-01-29 14:49:23 +0100

    Basic Gamma project.

---
+++ gamma/Cs/ShowCase/Program.cs
@@ -0,0 +1,29 @@
+﻿using Cs.AlphaLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+       do
+        {
+            string? input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input)) break;
+            Console.WriteLine($"Input: {input}");
+            var isDate = input.TryToDate(out DateTime dateIn);
+            string message = string.Empty;
+            if(!isDate)
+            {
+              message = $" {input} is not a valide date dd-mm-yyyy.";
+             Console.WriteLine(message);
+            }
+            else
+            {
+              int age = dateIn.GetAge();
+              Console.WriteLine($"Date of birth {dateIn.Date}, age is " + $"{(age)}.");
+
+            }
+        } while (true);
+        return;
+    }
+}
