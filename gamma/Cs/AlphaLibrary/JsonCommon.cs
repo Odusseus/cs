6b619e71946831b71fac5f656a8fd0f5d7dc8025
Pascal Boittin <pascalboittin@live.nl>
2022-02-18 17:29:21 +0100

    Added

---
+++ gamma/Cs/AlphaLibrary/JsonCommon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AlphaLibrary
+{
+    public class JsonCommon
+    {
+        public JsonCommon() { }
+        //https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-how-to?pivots=dotnet-6-0
+        public bool SaveJson<T>(string fileName, T data)
+        {
+            using FileStream createStream = File.Create(fileName);
+             JsonSerializer.Serialize(createStream, data);
+             createStream.Dispose();
+            return false;
+        }
+    }
+}
